c85b34b85fd3c0d35415cdc57abbaefbbd37d053
Corey Miller <coreyMiller353@gmail.com>
2020-04-26 23:22:37 -0400

    add functionality for adding budget items to UI

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,120 +1,166 @@
- var budgetController = (function() {
+var budgetController = (function () {
+	// Data Model for expenses and incomes
+	var Expense = function (id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
-  // Data Model for expenses and incomes
-  var Expense = function(id, description, value) {
-    this.id = id;
-    this.description = description;
-    this.value = value;
-  }
+	var Income = function (id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
 
-  var Income = function(id, description, value) {
-    this.id = id;
-    this.description = description;
-    this.value = value;
-  }
+	var data = {
+		allItems: {
+			exp: [],
+			inc: [],
+		},
+		totals: {
+			exp: 0,
+			inc: 0,
+		},
+	};
 
-  var data = {
-    allItems: {
-      exp: [],
-      inc: []
+	return {
+		addItem: function (type, des, val) {
+			var newItem;
+
+			// Create New ID
+			if (data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+			} else {
+				ID = 0;
+			}
+
+			// Create new item based on 'inc' or 'exp' type
+			if (type === "exp") {
+				newItem = new Expense(ID, des, val);
+			} else {
+				newItem = new Income(ID, des, val);
+			}
+			// Push into our data structure.
+			data.allItems[type].push(newItem);
+
+			// Return the new element.
+			return newItem;
+		},
+
+		testing: function () {
+			console.log(data);
+		},
+	};
+})();
+
+var UIController = (function () {
+	var DOMstrings = {
+		inputType: ".add__type",
+		inputDescription: ".add__description",
+		inputValue: ".add__value",
+    inputBtn: ".add__btn",
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
+	};
+
+	return {
+		getInput: function () {
+			return {
+				type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value,
+			};
+		},
+		addListItem: function (obj, type) {
+			var html, newHTML, element;
+			// Create HTML with placeholder text
+			if (type === "inc") {
+        element = DOMstrings.incomeContainer;
+				html = `
+      <div class="item clearfix" id="income-%id%">
+          <div class="item__description">%description%</div>
+          <div class="right clearfix">
+              <div class="item__value">%value%</div>
+              <div class="item__delete">
+                  <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+              </div>
+          </div>
+      </div>
+      `;
+			} else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
+				html = `
+        <div class="item clearfix" id="expense-%id%">
+            <div class="item__description">%description%</div>
+            <div class="right clearfix">
+                <div class="item__value">%value%</div>
+                <div class="item__percentage">21%</div>
+                <div class="item__delete">
+                    <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                </div>
+            </div>
+        </div>
+        `;
+			}
+
+			// replace the placeholder text, with actual data.
+      newHTML = html.replace('%id%', obj.id);
+      newHTML = newHTML.replace('%description%', obj.description);
+      newHTML = newHTML.replace('%value%', obj.value);
+
+      // insert the HTML into the DOM.
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
     },
-    totals: {
-      exp: 0,
-      inc: 0
-    }
-  }
+    clearFields: function() {
+      var fields, fieldsArr; 
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
 
-  return {
-    addItem: function(type, des, val) {
-      var newItem;
-      
-      // Create New ID
-      if (data.allItems[type].length > 0) {
-        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-      } else {
-        ID = 0;
-      }
-      
-      // Create new item based on 'inc' or 'exp' type
-      if (type === 'exp') {
-        newItem = new Expense( ID, des, val );
-      } else {
-        newItem = new Income( ID, des, val );
-      }
-      // Push into our data structure. 
-      data.allItems[type].push(newItem);
+      fieldsArr = Array.prototype.slice.call(fields); //OLD WAY OF DOING THIS, now use Array.from();
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = '';
+      });
 
-      // Return the new element. 
-      return newItem;
-
+      fieldsArr[0].focus();
     },
-    
-    testing: function() {
-      console.log(data);
-    }
-  };
+		getDOMstrings: function () {
+			return DOMstrings;
+		},
+	};
+})();
 
- })();
+var controller = (function (budgetCtrl, UICtrl) {
+	var setupEventListeners = function () {
+		var DOM = UICtrl.getDOMstrings();
 
- var UIController = (function() {
-  
-  var DOMstrings = {
-    inputType: '.add__type',
-    inputDescription: '.add__description',
-    inputValue: '.add__value',
-    inputBtn: '.add__btn'
-  }
+		document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
-  return {
-    getInput: function() {
-      return {
-        type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
-        description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
-      }
-    },
-    getDOMstrings: function() {
-      return DOMstrings;
-    }
-  }
+		document.addEventListener("keypress", function (event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
 
- })();
-
- var controller = (function(budgetCtrl, UICtrl) {
-  
-  var setupEventListeners = function() {
-    
-    var DOM = UICtrl.getDOMstrings();
-
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-      if (event.keyCode === 13 || event.which === 13) {
-        ctrlAddItem();
-      }
-    });
-  }
-
-
-
-  var ctrlAddItem = function() {
-    var input, newItem;
-    // 1. get the field input data
-    input = UICtrl.getInput();
-    // 2. add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+	var ctrlAddItem = function () {
+		var input, newItem;
+		// 1. get the field input data
+		input = UICtrl.getInput();
+		// 2. add the item to the budget controller
+		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. add the item to the UI
-    // 4. calculate the budget
-    // 5. Display the budget in the UI
-  };
+    UICtrl.addListItem(newItem,input.type);
+    // 4. Clear the fields
+    UICtrl.clearFields();
+		// 5. calculate the budget
+		// 6. Display the budget in the UI
+	};
 
-  return {
-    init: function() {
-      console.log('Application has started...');
-      setupEventListeners();
-    }
-  };
-  
- })(budgetController,UIController);
+	return {
+		init: function () {
+			console.log("Application has started...");
+			setupEventListeners();
+		},
+	};
+})(budgetController, UIController);
 
- controller.init();
+controller.init();
